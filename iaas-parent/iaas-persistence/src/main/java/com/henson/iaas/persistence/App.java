bb352fced6e6b66789f386770a056b14b0d738b0
huanghaoxing <279951090@qq.com>
2018-10-28 02:04:51 +0800

    初始化

---
+++ iaas-parent/iaas-persistence/src/main/java/com/henson/iaas/persistence/App.java
@@ -0,0 +1,13 @@
+package com.henson.iaas.persistence;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
